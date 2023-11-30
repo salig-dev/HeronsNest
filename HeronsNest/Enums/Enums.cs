bee3b044e20d8e5cfce129c5b69209f0ed5c1d57
JamTheDev <2004jamvillarosa@gmail.com>
2023-11-30 08:01:02 +0800

    Add project files.

---
+++ HeronsNest/Enums/Enums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeronsNest.Enums
+{
+    internal class Enums
+    {
+        public static int WINDOW_WIDTH = 1920;
+        public static int WINDOW_HEIGHT = 1080;
+    }
+}
---
+++ HeronsNest/Form1.cs
@@ -0,0 +1,10 @@
+namespace HeronsNest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
