4035d01e84aee3edc05b5c1194521d01ecc83763
Exequel <exql.adzn@gmail.com>
2023-12-21 20:16:02 +0800

    Add MyShelf inteface  - clean up guides

--- HeronsNest/Screens/MyShelf.Designer.cs
+++ HeronsNest/Screens/MyShelf.Designer.cs
@@ -33,34 +33,15 @@
             label1 = new Label();
             label3 = new Label();
             label4 = new Label();
-            pictureBox2 = new PictureBox();
-            pictureBox3 = new PictureBox();
-            pictureBox4 = new PictureBox();
-            pictureBox5 = new PictureBox();
-            pictureBox6 = new PictureBox();
-            pictureBox1 = new PictureBox();
             pictureBox9 = new PictureBox();
-            pictureBox10 = new PictureBox();
-            pictureBox11 = new PictureBox();
-            pictureBox12 = new PictureBox();
             pictureBox8 = new PictureBox();
-            pictureBox13 = new PictureBox();
-            pictureBox14 = new PictureBox();
-            pictureBox15 = new PictureBox();
             pictureBox7 = new PictureBox();
             button4 = new Button();
-            pictureBox16 = new PictureBox();
-            pictureBox17 = new PictureBox();
             button5 = new Button();
-            pictureBox18 = new PictureBox();
             label5 = new Label();
             pictureBox19 = new PictureBox();
             label6 = new Label();
             label7 = new Label();
-            pictureBox20 = new PictureBox();
-            pictureBox21 = new PictureBox();
-            pictureBox22 = new PictureBox();
-            pictureBox23 = new PictureBox();
             label8 = new Label();
             label9 = new Label();
             pictureBox24 = new PictureBox();
@@ -73,36 +54,17 @@
             label13 = new Label();
             button8 = new Button();
             button9 = new Button();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox9).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox10).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox11).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox12).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox13).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox14).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox15).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox16).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox17).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox18).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox19).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox20).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox21).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox22).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox23).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox24).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox25).BeginInit();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(42, 177);
+            button1.Location = new Point(55, 118);
             button1.Name = "button1";
             button1.Size = new Size(94, 29);
             button1.TabIndex = 0;
@@ -159,60 +121,6 @@
             label4.TabIndex = 5;
             label4.Text = "Borrowed Books";
             // 
-            // pictureBox2
-            // 
-            pictureBox2.BackColor = SystemColors.MenuHighlight;
-            pictureBox2.Location = new Point(0, 0);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(1000, 100);
-            pictureBox2.TabIndex = 9;
-            pictureBox2.TabStop = false;
-            // 
-            // pictureBox3
-            // 
-            pictureBox3.BackColor = SystemColors.MenuHighlight;
-            pictureBox3.Location = new Point(0, 580);
-            pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(1000, 20);
-            pictureBox3.TabIndex = 10;
-            pictureBox3.TabStop = false;
-            // 
-            // pictureBox4
-            // 
-            pictureBox4.BackColor = Color.Red;
-            pictureBox4.Location = new Point(196, 95);
-            pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(804, 20);
-            pictureBox4.TabIndex = 11;
-            pictureBox4.TabStop = false;
-            // 
-            // pictureBox5
-            // 
-            pictureBox5.BackColor = Color.Red;
-            pictureBox5.Location = new Point(196, 95);
-            pictureBox5.Name = "pictureBox5";
-            pictureBox5.Size = new Size(20, 600);
-            pictureBox5.TabIndex = 12;
-            pictureBox5.TabStop = false;
-            // 
-            // pictureBox6
-            // 
-            pictureBox6.BackColor = Color.Red;
-            pictureBox6.Location = new Point(980, 95);
-            pictureBox6.Name = "pictureBox6";
-            pictureBox6.Size = new Size(20, 600);
-            pictureBox6.TabIndex = 13;
-            pictureBox6.TabStop = false;
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.BackColor = Color.Red;
-            pictureBox1.Location = new Point(196, 206);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(804, 20);
-            pictureBox1.TabIndex = 14;
-            pictureBox1.TabStop = false;
-            // 
             // pictureBox9
             // 
             pictureBox9.Location = new Point(732, 258);
@@ -221,33 +129,6 @@
             pictureBox9.TabIndex = 17;
             pictureBox9.TabStop = false;
             // 
-            // pictureBox10
-            // 
-            pictureBox10.BackColor = Color.Red;
-            pictureBox10.Location = new Point(196, 232);
-            pictureBox10.Name = "pictureBox10";
-            pictureBox10.Size = new Size(804, 20);
-            pictureBox10.TabIndex = 18;
-            pictureBox10.TabStop = false;
-            // 
-            // pictureBox11
-            // 
-            pictureBox11.BackColor = Color.Red;
-            pictureBox11.Location = new Point(222, 232);
-            pictureBox11.Name = "pictureBox11";
-            pictureBox11.Size = new Size(20, 600);
-            pictureBox11.TabIndex = 19;
-            pictureBox11.TabStop = false;
-            // 
-            // pictureBox12
-            // 
-            pictureBox12.BackColor = Color.Red;
-            pictureBox12.Location = new Point(464, 232);
-            pictureBox12.Name = "pictureBox12";
-            pictureBox12.Size = new Size(20, 600);
-            pictureBox12.TabIndex = 20;
-            pictureBox12.TabStop = false;
-            // 
             // pictureBox8
             // 
             pictureBox8.Location = new Point(490, 258);
@@ -255,33 +136,6 @@
             pictureBox8.Size = new Size(210, 250);
             pictureBox8.TabIndex = 16;
             pictureBox8.TabStop = false;
-            // 
-            // pictureBox13
-            // 
-            pictureBox13.BackColor = Color.Red;
-            pictureBox13.Location = new Point(706, 232);
-            pictureBox13.Name = "pictureBox13";
-            pictureBox13.Size = new Size(20, 600);
-            pictureBox13.TabIndex = 21;
-            pictureBox13.TabStop = false;
-            // 
-            // pictureBox14
-            // 
-            pictureBox14.BackColor = Color.Red;
-            pictureBox14.Location = new Point(954, 232);
-            pictureBox14.Name = "pictureBox14";
-            pictureBox14.Size = new Size(20, 600);
-            pictureBox14.TabIndex = 22;
-            pictureBox14.TabStop = false;
-            // 
-            // pictureBox15
-            // 
-            pictureBox15.BackColor = Color.Red;
-            pictureBox15.Location = new Point(196, 488);
-            pictureBox15.Name = "pictureBox15";
-            pictureBox15.Size = new Size(804, 20);
-            pictureBox15.TabIndex = 23;
-            pictureBox15.TabStop = false;
             // 
             // pictureBox7
             // 
@@ -303,24 +157,6 @@
             button4.Text = "Return";
             button4.UseVisualStyleBackColor = false;
             // 
-            // pictureBox16
-            // 
-            pictureBox16.BackColor = Color.Red;
-            pictureBox16.Location = new Point(453, 232);
-            pictureBox16.Name = "pictureBox16";
-            pictureBox16.Size = new Size(5, 600);
-            pictureBox16.TabIndex = 25;
-            pictureBox16.TabStop = false;
-            // 
-            // pictureBox17
-            // 
-            pictureBox17.BackColor = Color.Red;
-            pictureBox17.Location = new Point(248, 232);
-            pictureBox17.Name = "pictureBox17";
-            pictureBox17.Size = new Size(5, 600);
-            pictureBox17.TabIndex = 26;
-            pictureBox17.TabStop = false;
-            // 
             // button5
             // 
             button5.BackColor = Color.FromArgb(0, 149, 168);
@@ -332,15 +168,6 @@
             button5.TabIndex = 27;
             button5.Text = "Borrowed";
             button5.UseVisualStyleBackColor = false;
-            // 
-            // pictureBox18
-            // 
-            pictureBox18.BackColor = Color.Red;
-            pictureBox18.Location = new Point(196, 258);
-            pictureBox18.Name = "pictureBox18";
-            pictureBox18.Size = new Size(804, 5);
-            pictureBox18.TabIndex = 28;
-            pictureBox18.TabStop = false;
             // 
             // label5
             // 
@@ -381,43 +208,6 @@
             label7.Size = new Size(154, 15);
             label7.TabIndex = 32;
             label7.Text = "Submission Due: {DATE}";
-            // 
-            // pictureBox20
-            // 
-            pictureBox20.BackColor = Color.Red;
-            pictureBox20.Location = new Point(490, 232);
-            pictureBox20.Name = "pictureBox20";
-            pictureBox20.Size = new Size(5, 600);
-            pictureBox20.TabIndex = 33;
-            pictureBox20.TabStop = false;
-            pictureBox20.Click += pictureBox20_Click;
-            // 
-            // pictureBox21
-            // 
-            pictureBox21.BackColor = Color.Red;
-            pictureBox21.Location = new Point(695, 232);
-            pictureBox21.Name = "pictureBox21";
-            pictureBox21.Size = new Size(5, 600);
-            pictureBox21.TabIndex = 34;
-            pictureBox21.TabStop = false;
-            // 
-            // pictureBox22
-            // 
-            pictureBox22.BackColor = Color.Red;
-            pictureBox22.Location = new Point(732, 232);
-            pictureBox22.Name = "pictureBox22";
-            pictureBox22.Size = new Size(5, 600);
-            pictureBox22.TabIndex = 35;
-            pictureBox22.TabStop = false;
-            // 
-            // pictureBox23
-            // 
-            pictureBox23.BackColor = Color.Red;
-            pictureBox23.Location = new Point(937, 232);
-            pictureBox23.Name = "pictureBox23";
-            pictureBox23.Size = new Size(5, 600);
-            pictureBox23.TabIndex = 36;
-            pictureBox23.TabStop = false;
             // 
             // label8
             // 
@@ -561,34 +351,15 @@
             Controls.Add(label10);
             Controls.Add(button6);
             Controls.Add(button7);
-            Controls.Add(pictureBox23);
-            Controls.Add(pictureBox22);
-            Controls.Add(pictureBox21);
-            Controls.Add(pictureBox20);
             Controls.Add(label7);
             Controls.Add(label6);
             Controls.Add(pictureBox19);
             Controls.Add(label5);
-            Controls.Add(pictureBox18);
             Controls.Add(button5);
-            Controls.Add(pictureBox17);
-            Controls.Add(pictureBox16);
             Controls.Add(button4);
-            Controls.Add(pictureBox15);
-            Controls.Add(pictureBox14);
-            Controls.Add(pictureBox13);
-            Controls.Add(pictureBox12);
-            Controls.Add(pictureBox11);
-            Controls.Add(pictureBox10);
             Controls.Add(pictureBox9);
             Controls.Add(pictureBox8);
             Controls.Add(pictureBox7);
-            Controls.Add(pictureBox1);
-            Controls.Add(pictureBox6);
-            Controls.Add(pictureBox5);
-            Controls.Add(pictureBox4);
-            Controls.Add(pictureBox3);
-            Controls.Add(pictureBox2);
             Controls.Add(label4);
             Controls.Add(label3);
             Controls.Add(label1);
@@ -597,29 +368,10 @@
             Name = "MyShelf";
             Size = new Size(1000, 600);
             Load += MyShelf_Load;
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox9).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox10).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox11).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox12).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox13).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox14).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox15).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox16).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox17).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox18).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox19).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox20).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox21).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox22).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox23).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox24).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox25).EndInit();
             ResumeLayout(false);
@@ -633,8 +385,6 @@
         private Label label1;
         private Label label3;
         private Label label4;
-        private PictureBox pictureBox2;
-        private PictureBox pictureBox3;
         private PictureBox pictureBox4;
         private PictureBox pictureBox5;
         private PictureBox pictureBox6;
