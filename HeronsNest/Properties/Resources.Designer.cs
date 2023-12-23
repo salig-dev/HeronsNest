fd1638d07e75dd66798d8f62910f95eb2ec89435
JamTheDev <2004jamvillarosa@gmail.com>
2023-12-23 21:08:17 +0800

    Fixed merging issues

--- HeronsNest/Properties/Resources.Designer.cs
+++ HeronsNest/Properties/Resources.Designer.cs
@@ -218,15 +218,5 @@ namespace HeronsNest.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap stars {
-            get {
-                object obj = ResourceManager.GetObject("stars", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
