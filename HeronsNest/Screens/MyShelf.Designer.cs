9c02e541da59d68b53b445d9519a9667e3e0bb83
Exequel <exql.adzn@gmail.com>
2023-12-26 13:47:44 +0800

    Edit BookCard component and MyShelf Layout  - Decrease BookCard siize

--- HeronsNest/Screens/MyShelf.Designer.cs
+++ HeronsNest/Screens/MyShelf.Designer.cs
@@ -31,16 +31,30 @@
             button1 = new Button();
             label2 = new Label();
             label3 = new Label();
-            bookCard1 = new Components.BookCard();
             label1 = new Label();
             label4 = new Label();
+            bookCard1 = new Components.BookCard();
             bookCard2 = new Components.BookCard();
-            sideNavigation1 = new Components.Navigation.SideNavigation();
+            bookCard3 = new Components.BookCard();
+            leftNavBar1 = new Components.LeftNavBar();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            bookCard4 = new Components.BookCard();
+            bookCard5 = new Components.BookCard();
+            bookCard6 = new Components.BookCard();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(348, 121);
+            button1.Location = new Point(364, 105);
             button1.Margin = new Padding(3, 2, 3, 2);
             button1.Name = "button1";
             button1.Size = new Size(82, 22);
@@ -57,7 +71,7 @@
             label2.Font = new Font("PP Hatton", 20.2499962F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label2.ForeColor = Color.FromArgb(0, 149, 168);
             label2.ImageAlign = ContentAlignment.MiddleLeft;
-            label2.Location = new Point(203, 119);
+            label2.Location = new Point(219, 103);
             label2.Margin = new Padding(0);
             label2.Name = "label2";
             label2.Size = new Size(142, 32);
@@ -71,18 +85,11 @@
             label3.Font = new Font("PP Hatton", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label3.ForeColor = Color.FromArgb(76, 76, 76);
             label3.ImageAlign = ContentAlignment.MiddleLeft;
-            label3.Location = new Point(587, 176);
+            label3.Location = new Point(603, 150);
             label3.Name = "label3";
             label3.Size = new Size(145, 19);
             label3.TabIndex = 4;
             label3.Text = "Pending Books";
-            // 
-            // bookCard1
-            // 
-            bookCard1.Location = new Point(203, 225);
-            bookCard1.Name = "bookCard1";
-            bookCard1.Size = new Size(316, 300);
-            bookCard1.TabIndex = 7;
             // 
             // label1
             // 
@@ -90,7 +97,7 @@
             label1.Font = new Font("PP Hatton", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.ForeColor = Color.FromArgb(76, 76, 76);
             label1.ImageAlign = ContentAlignment.MiddleLeft;
-            label1.Location = new Point(362, 177);
+            label1.Location = new Point(378, 151);
             label1.Name = "label1";
             label1.Size = new Size(163, 19);
             label1.TabIndex = 13;
@@ -102,26 +109,106 @@
             label4.Font = new Font("PP Hatton", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label4.ForeColor = Color.FromArgb(76, 76, 76);
             label4.ImageAlign = ContentAlignment.MiddleLeft;
-            label4.Location = new Point(206, 177);
+            label4.Location = new Point(222, 151);
             label4.Name = "label4";
             label4.Size = new Size(94, 19);
             label4.TabIndex = 14;
             label4.Text = "All Books";
             // 
+            // bookCard1
+            // 
+            bookCard1.Location = new Point(222, 199);
+            bookCard1.Name = "bookCard1";
+            bookCard1.Size = new Size(210, 200);
+            bookCard1.TabIndex = 19;
+            // 
             // bookCard2
             // 
-            bookCard2.Location = new Point(525, 225);
+            bookCard2.Location = new Point(438, 199);
             bookCard2.Name = "bookCard2";
-            bookCard2.Size = new Size(316, 300);
-            bookCard2.TabIndex = 18;
+            bookCard2.Size = new Size(210, 200);
+            bookCard2.TabIndex = 20;
             // 
-            // sideNavigation1
+            // bookCard3
             // 
-            sideNavigation1.BackColor = Color.Transparent;
-            sideNavigation1.Location = new Point(0, 0);
-            sideNavigation1.Name = "sideNavigation1";
-            sideNavigation1.Size = new Size(200, 600);
-            sideNavigation1.TabIndex = 6;
+            bookCard3.Location = new Point(654, 199);
+            bookCard3.Name = "bookCard3";
+            bookCard3.Size = new Size(210, 200);
+            bookCard3.TabIndex = 21;
+            // 
+            // leftNavBar1
+            // 
+            leftNavBar1.BackColor = Color.White;
+            leftNavBar1.Location = new Point(0, 0);
+            leftNavBar1.Name = "leftNavBar1";
+            leftNavBar1.Size = new Size(200, 600);
+            leftNavBar1.TabIndex = 22;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Red;
+            pictureBox1.Location = new Point(206, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(10, 600);
+            pictureBox1.TabIndex = 23;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.Red;
+            pictureBox2.Location = new Point(987, 0);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(10, 600);
+            pictureBox2.TabIndex = 24;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = Color.Red;
+            pictureBox3.Location = new Point(206, 0);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(1000, 100);
+            pictureBox3.TabIndex = 25;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackColor = Color.Red;
+            pictureBox4.Location = new Point(-19, 138);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(1000, 10);
+            pictureBox4.TabIndex = 26;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.BackColor = Color.Red;
+            pictureBox5.Location = new Point(-3, 173);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(1000, 20);
+            pictureBox5.TabIndex = 27;
+            pictureBox5.TabStop = false;
+            // 
+            // bookCard4
+            // 
+            bookCard4.Location = new Point(654, 405);
+            bookCard4.Name = "bookCard4";
+            bookCard4.Size = new Size(210, 200);
+            bookCard4.TabIndex = 30;
+            // 
+            // bookCard5
+            // 
+            bookCard5.Location = new Point(438, 405);
+            bookCard5.Name = "bookCard5";
+            bookCard5.Size = new Size(210, 200);
+            bookCard5.TabIndex = 29;
+            // 
+            // bookCard6
+            // 
+            bookCard6.Location = new Point(222, 405);
+            bookCard6.Name = "bookCard6";
+            bookCard6.Size = new Size(210, 200);
+            bookCard6.TabIndex = 28;
             // 
             // MyShelf
             // 
@@ -129,11 +216,20 @@
             AutoScaleMode = AutoScaleMode.Font;
             AutoSizeMode = AutoSizeMode.GrowAndShrink;
             BackColor = Color.FromArgb(243, 243, 247);
+            Controls.Add(bookCard4);
+            Controls.Add(bookCard5);
+            Controls.Add(bookCard6);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(leftNavBar1);
+            Controls.Add(bookCard3);
             Controls.Add(bookCard2);
+            Controls.Add(bookCard1);
             Controls.Add(label4);
             Controls.Add(label1);
-            Controls.Add(bookCard1);
-            Controls.Add(sideNavigation1);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(button1);
@@ -141,6 +237,11 @@
             Name = "MyShelf";
             Size = new Size(1000, 600);
             Load += MyShelf_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -166,10 +267,16 @@
         private Components.BookCard bookCard4;
         private Components.BookCard bookCard5;
         private Components.BookCard bookCard6;
-        private Components.BookCard bookCard1;
         private Label label1;
         private Label label4;
+        private Components.BookCard bookCard1;
         private Components.BookCard bookCard2;
-        private Components.Navigation.SideNavigation sideNavigation1;
+        private Components.BookCard bookCard3;
+        private Components.LeftNavBar leftNavBar1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
     }
 }
