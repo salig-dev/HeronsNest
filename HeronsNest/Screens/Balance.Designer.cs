277213f7469cce39dd8f4fe54b753ffa3c01a5bc
Exequel <exql.adzn@gmail.com>
2023-12-27 19:59:09 +0800

    Add layout in Balance Screen

--- HeronsNest/Screens/Balance.Designer.cs
+++ HeronsNest/Screens/Balance.Designer.cs
@@ -30,14 +30,12 @@
         {
             leftNavBar1 = new Components.LeftNavBar();
             label1 = new Label();
-            pictureBox1 = new PictureBox();
-            pictureBox2 = new PictureBox();
-            pictureBox3 = new PictureBox();
-            pictureBox4 = new PictureBox();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            balanceBookCard1 = new Components.BalanceBookCard();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            balanceBookCard2 = new Components.BalanceBookCard();
             SuspendLayout();
             // 
             // leftNavBar1
@@ -54,65 +52,86 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("PP Hatton", 20.2499962F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(0, 149, 168);
             label1.Location = new Point(231, 156);
             label1.Name = "label1";
             label1.Size = new Size(284, 32);
             label1.TabIndex = 1;
             label1.Text = "Pending Payments";
             // 
-            // pictureBox1
+            // balanceBookCard1
             // 
-            pictureBox1.BackColor = Color.Red;
-            pictureBox1.Location = new Point(200, 0);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(800, 150);
-            pictureBox1.TabIndex = 2;
-            pictureBox1.TabStop = false;
+            balanceBookCard1.Location = new Point(276, 244);
+            balanceBookCard1.Name = "balanceBookCard1";
+            balanceBookCard1.Size = new Size(658, 150);
+            balanceBookCard1.TabIndex = 6;
             // 
-            // pictureBox2
+            // label2
             // 
-            pictureBox2.BackColor = Color.Red;
-            pictureBox2.Location = new Point(200, 156);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(25, 441);
-            pictureBox2.TabIndex = 3;
-            pictureBox2.TabStop = false;
+            label2.AutoSize = true;
+            label2.Font = new Font("PP Hatton", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.FromArgb(76, 76, 76);
+            label2.Location = new Point(412, 214);
+            label2.Name = "label2";
+            label2.Size = new Size(49, 19);
+            label2.TabIndex = 7;
+            label2.Text = "Title";
             // 
-            // pictureBox3
+            // label3
             // 
-            pictureBox3.BackColor = Color.Red;
-            pictureBox3.Location = new Point(972, 159);
-            pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(25, 441);
-            pictureBox3.TabIndex = 4;
-            pictureBox3.TabStop = false;
+            label3.AutoSize = true;
+            label3.Font = new Font("PP Hatton", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.FromArgb(76, 76, 76);
+            label3.Location = new Point(553, 214);
+            label3.Name = "label3";
+            label3.Size = new Size(66, 19);
+            label3.TabIndex = 10;
+            label3.Text = "Status";
             // 
-            // pictureBox4
+            // label4
             // 
-            pictureBox4.BackColor = Color.Red;
-            pictureBox4.Location = new Point(200, 191);
-            pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(800, 20);
-            pictureBox4.TabIndex = 5;
-            pictureBox4.TabStop = false;
+            label4.AutoSize = true;
+            label4.Font = new Font("PP Hatton", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.FromArgb(76, 76, 76);
+            label4.Location = new Point(651, 214);
+            label4.Name = "label4";
+            label4.Size = new Size(74, 19);
+            label4.TabIndex = 12;
+            label4.Text = "Penalty";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("PP Hatton", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.FromArgb(76, 76, 76);
+            label5.Location = new Point(747, 214);
+            label5.Name = "label5";
+            label5.Size = new Size(53, 19);
+            label5.TabIndex = 14;
+            label5.Text = "Total";
+            // 
+            // balanceBookCard2
+            // 
+            balanceBookCard2.Location = new Point(276, 400);
+            balanceBookCard2.Name = "balanceBookCard2";
+            balanceBookCard2.Size = new Size(658, 150);
+            balanceBookCard2.TabIndex = 15;
             // 
             // Balance
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(246, 246, 246);
-            Controls.Add(pictureBox4);
-            Controls.Add(pictureBox3);
-            Controls.Add(pictureBox2);
-            Controls.Add(pictureBox1);
+            Controls.Add(balanceBookCard2);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(balanceBookCard1);
             Controls.Add(label1);
             Controls.Add(leftNavBar1);
             Name = "Balance";
             Size = new Size(1000, 600);
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -121,9 +140,11 @@
 
         private Components.LeftNavBar leftNavBar1;
         private Label label1;
-        private PictureBox pictureBox1;
-        private PictureBox pictureBox2;
-        private PictureBox pictureBox3;
-        private PictureBox pictureBox4;
+        private Components.BalanceBookCard balanceBookCard1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Components.BalanceBookCard balanceBookCard2;
     }
 }
