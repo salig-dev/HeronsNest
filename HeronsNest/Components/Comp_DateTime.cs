435141a238e8ac96a343c7747c335bc1719265f3
Mark Siazon <markangelosiazon@gmail.com>
2023-12-29 17:01:31 +0800

    Update: Organize by separating components into 2. - Also remove unused functions in searchbar - Move function from searchbar to datetime

---
+++ HeronsNest/Components/Comp_DateTime.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HeronsNest.Components
+{
+    public partial class Comp_DateTime : UserControl
+    {
+        public Comp_DateTime()
+        {
+            InitializeComponent();
+            timeText.Text = "....";
+            dateText.Text = "-- - -- - --";
+        }
+
+        private void Comp_DataTime_Load(object sender, EventArgs e)
+        {
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    await Task.Delay(1000);
+                    DateTime currentTime = DateTime.Now;
+                    Invoke((MethodInvoker)delegate
+                    {
+                        timeText.Text = $"{(currentTime.Hour <= 9 ? "0" + currentTime.Hour : currentTime.Hour)}:" +
+                        $"{(currentTime.Minute <= 9 ? "0" + currentTime.Minute : currentTime.Minute)}:" +
+                        $"{(currentTime.Second <= 9 ? "0" + currentTime.Second : currentTime.Second)}";
+                        dateText.Text = $"{(currentTime.Day <= 9 ? "0" + currentTime.Day : currentTime.Day)} - {(currentTime.Month <= 9 ? "0" + currentTime.Month : currentTime.Month)} - {(currentTime.Year <= 9 ? "0" + currentTime.Year : currentTime.Year)}";
+                    });
+                }
+            });
+        }
+    }
+}
--- HeronsNest/Components/Comp_Searchbar.Designer.cs
+++ HeronsNest/Components/Comp_Searchbar.Designer.cs
@@ -34,22 +34,14 @@
             txtSearch = new TextBox();
             btnSearch = new Button();
             panel1 = new Panel();
-            panel2 = new Panel();
-            dateText = new Label();
-            pictureBox2 = new PictureBox();
-            timeText = new Label();
-            pictureBox1 = new PictureBox();
             panel1.SuspendLayout();
-            panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // cmbCategory
             // 
             cmbCategory.FormattingEnabled = true;
             cmbCategory.Items.AddRange(new object[] { "Category1", "Category2", "Category3" });
-            cmbCategory.Location = new Point(12, 14);
+            cmbCategory.Location = new Point(9, 6);
             cmbCategory.Margin = new Padding(3, 2, 3, 2);
             cmbCategory.Name = "cmbCategory";
             cmbCategory.Size = new Size(64, 23);
@@ -59,7 +51,7 @@
             // txtSearch
             // 
             txtSearch.BorderStyle = BorderStyle.None;
-            txtSearch.Location = new Point(15, 18);
+            txtSearch.Location = new Point(12, 18);
             txtSearch.Margin = new Padding(3, 2, 3, 2);
             txtSearch.Name = "txtSearch";
             txtSearch.PlaceholderText = "Search";
@@ -73,7 +65,7 @@
             btnSearch.FlatStyle = FlatStyle.Flat;
             btnSearch.ForeColor = SystemColors.ButtonHighlight;
             btnSearch.Image = (Image)resources.GetObject("btnSearch.Image");
-            btnSearch.Location = new Point(268, 16);
+            btnSearch.Location = new Point(268, 15);
             btnSearch.Margin = new Padding(3, 2, 3, 2);
             btnSearch.Name = "btnSearch";
             btnSearch.Size = new Size(24, 20);
@@ -87,72 +79,11 @@
             panel1.BackgroundImage = (Image)resources.GetObject("panel1.BackgroundImage");
             panel1.Controls.Add(btnSearch);
             panel1.Controls.Add(txtSearch);
-            panel1.Location = new Point(79, -2);
+            panel1.Location = new Point(76, -8);
             panel1.MaximumSize = new Size(300, 48);
             panel1.Name = "panel1";
-            panel1.Size = new Size(300, 41);
+            panel1.Size = new Size(300, 43);
             panel1.TabIndex = 4;
-            // 
-            // panel2
-            // 
-            panel2.BackColor = Color.Transparent;
-            panel2.BackgroundImage = (Image)resources.GetObject("panel2.BackgroundImage");
-            panel2.Controls.Add(dateText);
-            panel2.Controls.Add(pictureBox2);
-            panel2.Controls.Add(timeText);
-            panel2.Controls.Add(pictureBox1);
-            panel2.Location = new Point(370, 5);
-            panel2.MaximumSize = new Size(300, 48);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(241, 35);
-            panel2.TabIndex = 5;
-            // 
-            // dateText
-            // 
-            dateText.AutoSize = true;
-            dateText.BackColor = Color.Transparent;
-            dateText.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            dateText.Location = new Point(137, 13);
-            dateText.Name = "dateText";
-            dateText.Size = new Size(80, 15);
-            dateText.TabIndex = 3;
-            dateText.Text = "27 - 11 - 2023";
-            dateText.TextAlign = ContentAlignment.MiddleCenter;
-            dateText.Click += label1_Click_1;
-            // 
-            // pictureBox2
-            // 
-            pictureBox2.BackgroundImageLayout = ImageLayout.None;
-            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
-            pictureBox2.Location = new Point(111, 10);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(26, 20);
-            pictureBox2.TabIndex = 2;
-            pictureBox2.TabStop = false;
-            pictureBox2.Click += pictureBox2_Click;
-            // 
-            // timeText
-            // 
-            timeText.AutoSize = true;
-            timeText.BackColor = Color.Transparent;
-            timeText.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            timeText.Location = new Point(50, 12);
-            timeText.Name = "timeText";
-            timeText.Size = new Size(38, 15);
-            timeText.TabIndex = 1;
-            timeText.Text = "00:00";
-            timeText.TextAlign = ContentAlignment.MiddleCenter;
-            timeText.Click += label1_Click;
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.BackgroundImageLayout = ImageLayout.None;
-            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(27, 10);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(20, 20);
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
             // 
             // Comp_Searchbar
             // 
@@ -160,17 +91,11 @@
             AutoScaleMode = AutoScaleMode.Font;
             Controls.Add(panel1);
             Controls.Add(cmbCategory);
-            Controls.Add(panel2);
             Margin = new Padding(3, 2, 3, 2);
             Name = "Comp_Searchbar";
-            Size = new Size(641, 44);
-            Load += Comp_Searchbar_Load;
+            Size = new Size(382, 35);
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
-            panel2.ResumeLayout(false);
-            panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
@@ -186,10 +111,5 @@
         }
 
         private Panel panel1;
-        private Panel panel2;
-        private PictureBox pictureBox1;
-        private Label timeText;
-        private Label dateText;
-        private PictureBox pictureBox2;
     }
 }
