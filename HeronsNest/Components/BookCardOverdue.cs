e59cd2b105244a940cd363864586312040bd2cda
Exequel <exql.adzn@gmail.com>
2024-01-05 10:15:46 +0800

    Connect BookCardOverdue info from DB

--- HeronsNest/Components/BookCardOverdue.cs
+++ HeronsNest/Components/BookCardOverdue.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using HeronsNest.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,38 @@ namespace HeronsNest.Components
 {
     public partial class BookCardOverdue : UserControl
     {
-        public BookCardOverdue()
+
+        private readonly BookReservation BookReservation;
+        private readonly Book Book;
+
+        public BookCardOverdue(BookReservation bookReservation, Book book)
         {
             InitializeComponent();
+
+            BookReservation = bookReservation;
+            Book = book;
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            borrowedDate.Text = DateTime.Parse(BookReservation.DateReserved).ToShortDateString();
+            returnDate.Text = DateTime.Parse(BookReservation.DateReturn).ToShortDateString();
+
+            bookAuthor.Text = Book.Author;
+            bookTitle.Text = Book.Title;
+
+            // TODO: LOAD IMAGE
+            try
+            {
+                bookImage.LoadAsync(Book.CoverImg);
+            }
+            catch
+            {
+                Debug.WriteLine($"{Book.Isbn} does not have a proper image path!");
+            }
+
         }
     }
 }
