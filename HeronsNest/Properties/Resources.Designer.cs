ca754f3c88d23028bd92c9e3b28aacb9a3232ebb
Exequel <exql.adzn@gmail.com>
2024-01-09 15:08:06 +0800

    Update CategoryListItem Design

--- HeronsNest/Properties/Resources.Designer.cs
+++ HeronsNest/Properties/Resources.Designer.cs
@@ -113,6 +113,16 @@ namespace HeronsNest.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap bookCardHome {
+            get {
+                object obj = ResourceManager.GetObject("bookCardHome", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap card_active {
             get {
                 object obj = ResourceManager.GetObject("card_active", resourceCulture);
